8e30639f9a76ec49544a9483781877c08664e68a
HcwXd <e04dl47878@gmail.com>
2018-06-10 21:15:33 +0800

    Upload views

--- Blog/app.js
+++ Blog/app.js
@@ -35,6 +35,20 @@ app.use(flash())
 // 路由
 routes(app)
 
+// 设置模板全局常量
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+}
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user
+  res.locals.success = req.flash('success').toString()
+  res.locals.error = req.flash('error').toString()
+  next()
+})
+
 // 监听端口，启动程序
 app.listen(config.port, function () {
   console.log(`${pkg.name} listening on port ${config.port}`)
