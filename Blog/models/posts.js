652ce996044b5282be29975c32102660b28acdee
HcwXd <e04dl47878@gmail.com>
2018-06-11 01:55:49 +0800

    Finish the function of adding a post

--- Blog/models/posts.js
+++ Blog/models/posts.js
@@ -41,15 +41,20 @@ Post.plugin('contentToHtml', {
 
 module.exports = {
   // 创建一篇文章
-  create: function create (post) {
+  create: function create(post) {
     return Post.create(post).exec()
   },
 
   // 通过文章 id 获取一篇文章
-  getPostById: function getPostById (postId) {
+  getPostById: function getPostById(postId) {
     return Post
-      .findOne({ _id: postId })
-      .populate({ path: 'author', model: 'User' })
+      .findOne({
+        _id: postId
+      })
+      .populate({
+        path: 'author',
+        model: 'User'
+      })
       .addCreatedAt()
       .addCommentsCount()
       .contentToHtml()
@@ -57,15 +62,20 @@ module.exports = {
   },
 
   // 按创建时间降序获取所有用户文章或者某个特定用户的所有文章
-  getPosts: function getPosts (author) {
+  getPosts: function getPosts(author) {
     const query = {}
     if (author) {
       query.author = author
     }
     return Post
       .find(query)
-      .populate({ path: 'author', model: 'User' })
-      .sort({ _id: -1 })
+      .populate({
+        path: 'author',
+        model: 'User'
+      })
+      .sort({
+        _id: -1
+      })
       .addCreatedAt()
       .addCommentsCount()
       .contentToHtml()
@@ -73,28 +83,45 @@ module.exports = {
   },
 
   // 通过文章 id 给 pv 加 1
-  incPv: function incPv (postId) {
+  incPv: function incPv(postId) {
     return Post
-      .update({ _id: postId }, { $inc: { pv: 1 } })
+      .update({
+        _id: postId
+      }, {
+        $inc: {
+          pv: 1
+        }
+      })
       .exec()
   },
 
   // 通过文章 id 获取一篇原生文章（编辑文章）
-  getRawPostById: function getRawPostById (postId) {
+  getRawPostById: function getRawPostById(postId) {
     return Post
-      .findOne({ _id: postId })
-      .populate({ path: 'author', model: 'User' })
+      .findOne({
+        _id: postId
+      })
+      .populate({
+        path: 'author',
+        model: 'User'
+      })
       .exec()
   },
 
   // 通过文章 id 更新一篇文章
-  updatePostById: function updatePostById (postId, data) {
-    return Post.update({ _id: postId }, { $set: data }).exec()
+  updatePostById: function updatePostById(postId, data) {
+    return Post.update({
+      _id: postId
+    }, {
+      $set: data
+    }).exec()
   },
 
   // 通过文章 id 删除一篇文章
-  delPostById: function delPostById (postId) {
-    return Post.deleteOne({ _id: postId })
+  delPostById: function delPostById(postId) {
+    return Post.deleteOne({
+        _id: postId
+      })
       .exec()
       .then(function (res) {
         // 文章删除后，再删除该文章下的所有留言
