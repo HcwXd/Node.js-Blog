0b619d2688c667987a97dbd72ea79f5ace1dccf4
HcwXd <e04dl47878@gmail.com>
2018-06-14 18:05:58 +0800

    Update server side comment

--- Blog/app.js
+++ Blog/app.js
@@ -12,38 +12,37 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-// 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')))
 
-// session 中间件
+// Session setting
 app.use(session({
-  name: config.session.key, // 设置 cookie 中保存 session id 的字段名称
-  secret: config.session.secret, // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
-  resave: true, // 强制更新 session
-  saveUninitialized: false, // 设置为 false，强制创建一个 session，即使用户未登录
+  name: config.session.key, // Session name = config's key
+  secret: config.session.secret, // Use secret parameter to protect signedCookie from being revised
+  resave: true, // Refresh session
+  saveUninitialized: false, // Generate session even user haven't signed in
   cookie: {
-    maxAge: config.session.maxAge // 过期时间，过期后 cookie 中的 session id 自动删除
+    maxAge: config.session.maxAge // Delete session id when dued
   },
-  store: new MongoStore({ // 将 session 存储到 mongodb
-    url: config.mongodb // mongodb 地址
+  store: new MongoStore({ // Store session to mongod
+    url: config.mongodb
   })
 }))
-// flash 中间件，用来显示通知
+// Use flash middleware to get notification
 app.use(flash())
 
-// 处理表单及文件上传的中间件
+// Use express-formidable to handle uploaded files
 app.use(require('express-formidable')({
-  uploadDir: path.join(__dirname, 'public/images'), // 上传文件目录
-  keepExtensions: true // 保留后缀
+  uploadDir: path.join(__dirname, 'public/images'),
+  keepExtensions: true // Keep postfix
 }))
 
-// 设置模板全局常量
+// Set variables for template
 app.locals.blog = {
   title: pkg.name,
   description: pkg.description
 }
 
-// 添加模板必需的三个变量
+// Add three required variables to the template
 app.use(function (req, res, next) {
   res.locals.user = req.session.user
   res.locals.success = req.flash('success').toString()
@@ -51,10 +50,9 @@ app.use(function (req, res, next) {
   next()
 })
 
-// 路由
 routes(app)
 
-// 监听端口，启动程序
+// listen on port number from config file and log the app name from package.json 
 app.listen(config.port, function () {
   console.log(`${pkg.name} listening on port ${config.port}`)
 })
